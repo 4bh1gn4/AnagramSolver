20caaf8c9db17aa0aedab0d49152d370c980fe79
an14uw <abhignan6@gmail.com>
2024-08-23 01:55:35 -0700

    Enabled next functionality. Fixing minor comparison bugs

--- AnagramSolver/src/application/AnagramUtils.java
+++ AnagramSolver/src/application/AnagramUtils.java
@@ -1,6 +1,7 @@
 package application;
 
 //import java.util.random;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -8,19 +9,35 @@ import java.util.List;
 public class AnagramUtils {
 	private static final List<String> words = Arrays.asList("hello", "type", "stop", "phone");
     
-	public static String getRandomWord() {
-		Collections.shuffle(words); //this way, i do not need to access a word from a random array index
-		return words.get(0);
+	public static String getWord(int index) {
+		return words.get(index);
+	}
+	
+	public static int getWordCount() {
+		return words.size() - 1;
+	}
+	public static String getRandomWord(int index) {
+		
+		//Collections.shuffle(words); //this way, i do not need to access a word from a random array index
+		return words.get(index);
 	}
 	
 	public static String jumbleWord(String word) {
-		List<char[]> letters = Arrays.asList(word.toCharArray());
+		List<Character> letters = new ArrayList<Character>();
+		for (char c : word.toCharArray()) {
+			letters.add(c);
+		}
 		Collections.shuffle(letters);
 		
-		StringBuilder jumbledWord = new StringBuilder();
+		String jumbledWord = new String("");
 		
-		for (char[] letter : letters) {
-			jumbledWord.append(letter);
+		while (!jumbledWord.equalsIgnoreCase(word)) {
+			
+		
+			for (char letter : letters) {
+				//System.out.print(letter + " ");
+				jumbledWord+=letter;
+			}
 		}
 		
 		return jumbledWord.toString();
